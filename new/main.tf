75c90fc7d88b70588a5c8522abf3e93898b8c601
Matt Godbolt <matt@godbolt.org>
2024-03-17 18:52:56 -0500

    Videos and music

--- new/main.tf
+++ new/main.tf
@@ -28,3 +28,31 @@ data "aws_caller_identity" "this" {}
 resource "aws_s3_bucket" "music" {
   bucket = "music.xania.org"
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "music" {
+  bucket = aws_s3_bucket.music.bucket
+  rule {
+    id = "ensure_intelligent"
+        status = "Enabled"
+    transition {
+      days = 0
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "videos" {
+  bucket = "videos.xania.org"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "videos" {
+  bucket = aws_s3_bucket.videos.bucket
+  rule {
+    id = "ensure_intelligent"
+        status = "Enabled"
+    transition {
+      days = 0
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+}
